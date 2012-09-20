9c6d12ba672fb7f82fff62221f627061d1e367b3
meggy meng <meggy.meng@gmail.com>
2012-09-20 11:22:07 +0800

    Initial Commit

---
+++ testgit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testgit
+//
+//  Created by fanting meng on 12-9-20.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
